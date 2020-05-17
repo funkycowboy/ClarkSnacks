c29a5d384bd59a4876a1577e88774d499bc62bdf
Aaron Gatlin <aaron.gatlin@getbeyond.com>
2020-05-17 16:51:38 -0400

    checkin new projects: api, domain, EF

---
+++ ClarkSnacks.MTS.EntityFramework/DependencyInjection/ContextInjector.cs
@@ -0,0 +1,21 @@
+﻿using ClarkSnacks.MTS.EntityFramework.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BeyondService.EntityFramework.DependencyInjection
+{
+    public static class ContextInjector
+    {
+        //public static void ConfigureEntityFramework(IServiceCollection services, string connectionString, string environment)
+        //{
+        //    if (environment.ToLower() != "production")
+        //        services.AddDbContext<MTSDbContext>(options => options.UseSqlServer(connectionString)
+        //            .EnableDetailedErrors()
+        //            .EnableSensitiveDataLogging());
+        //    else
+        //        services.AddDbContext<MTSDbContext>(options => options.UseSqlServer(connectionString));
+        //}
+    }
+}
+
+
