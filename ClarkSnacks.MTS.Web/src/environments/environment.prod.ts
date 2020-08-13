f3743559c54b6ac985e2ac72e4b5767ba22a7d6e
Aaron Gatlin <agatlin3@hotmail.com>
2020-08-13 10:36:03 -0400

    update budget amount

--- ClarkSnacks.MTS.Web/src/environments/environment.prod.ts
+++ ClarkSnacks.MTS.Web/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     clarkSnacksUrl: "https://clarksnacks-api.azurewebsites.net/api/",
-    auth0CallbackUrl: "https://clarksnacks.azurewebsites.net/",
+    auth0CallbackUrl: "https://clarksnacks.azurewebsites.net",
     auth0RedirectUrl: "https://clarksnacks.azurewebsites.net/callback"
 };
